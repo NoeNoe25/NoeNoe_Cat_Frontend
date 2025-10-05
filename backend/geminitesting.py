8d5ac6201d4c9e49cf53b1c5a7881b3b726da256
Hsu Myat Noe <hsumyatnoe031@gmail.com>
2025-10-05 18:55:46 +0700

    Frontend chat, register and login form

--- backend/geminitesting.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBqpN-lzZ4ePx99up3DpAaGZ1f6ChWS4vg")
-
-# List all models available to your API key
-models = genai.list_models()
-print(models)
